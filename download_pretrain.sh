6b2dfaf7324fc9beb17880b5705b10fcd4f8eaf0
Berry Weinstein <35626084+berryweinst@users.noreply.github.com>
2018-05-14 22:29:23 +0300

    Add files via upload

---
+++ download_pretrain.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+wget http://netdissect.csail.mit.edu/dissect/zoo/caffe_reference_places365.caffemodel -P model
+wget http://netdissect.csail.mit.edu/dissect/zoo/caffe_reference_places365.prototxt -P model
+wget http://netdissect.csail.mit.edu/dissect/zoo/caffe_reference_imagenet.caffemodel -P model
+wget http://netdissect.csail.mit.edu/dissect/zoo/caffe_reference_imagenet.prototxt -P model
+#wget http://netdissect.csail.mit.edu/dissect/zoo/vgg16_places365.caffemodel -P model
+#wget http://netdissect.csail.mit.edu/dissect/zoo/vgg16_places365.prototxt -P model
